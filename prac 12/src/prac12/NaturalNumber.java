809329d5d2392d698495c45a82e13b3d51db3b42
nandani109 <nandanisolanki1810@gmail.com>
2024-03-13 15:17:44 +0530

    prac12

---
+++ prac 12/src/prac12/NaturalNumber.java
@@ -0,0 +1,11 @@
+package prac12;
+public class NaturalNumber {
+    public static void main(String[] args) {
+        int num = 1;
+        System.out.println("First 10 Natural Numbers are: ");
+        while (num <= 10){
+            System.out.print(num+" ");
+            num++;
+        }
+    }
+}
