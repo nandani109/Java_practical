ca5e7bc0a5a4544e21732ba66255d8aeadb55ccc
nandani109 <nandanisolanki1810@gmail.com>
2024-03-30 16:58:52 +0530

    prac26

---
+++ prac 26/src/prac26/P26.java
@@ -0,0 +1,15 @@
+package prac26;
+public class P26 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        try{
+        arr[6]=55;
+        }
+        catch(Exception e){
+            System.out.println(e);
+            e.printStackTrace();
+        }
+    }
+    
+}
