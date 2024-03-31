e5faaf34ee934a9f2821d7a7d823a9cde04b6f74
nandani109 <nandanisolanki1810@gmail.com>
2024-03-31 17:40:02 +0530

    prac31

---
+++ prac31/src/prac31/main.java
@@ -0,0 +1,25 @@
+package prac31;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double num1 = -2.34;
+		System.out.println("Absolute value of -2.34 is "+ Math.abs(num1));
+		
+		int num2 = 4;
+		int num3 = 1;
+		System.out.println("Greatest number between 4 and 1 is "+ Math.max(num2, num3));
+		
+		int num4 = 4;
+		int num5 = 1;
+		System.out.println("Greatest number between 4 and 1 is "+ Math.min(num2, num3));
+		
+		double num6 = 36;
+		System.out.println("Sqaure root of 36 is "+ Math.sqrt(num6));
+		
+		double num7 = 12.34445;
+		System.out.println("Round of 12.34445 is "+ Math.round(num7));
+	}
+
+}
