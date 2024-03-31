5080dcfe9fdaea6d31a894a3ae1b3ce37857420e
nandani109 <nandanisolanki1810@gmail.com>
2024-03-31 17:42:46 +0530

    prac32

---
+++ prac 32/src/prac32/main.java
@@ -0,0 +1,15 @@
+package prac32;
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "Hello World";
+		
+		System.out.println("Length od String is "+ s.length());
+		System.out.println("Character at index 8 is "+ s.charAt(8));
+		System.out.println("String in Upper Case: "+ s.toUpperCase());
+		System.out.println("Index of 'r': "+ s.indexOf('r'));
+		System.out.println("Replace H with M: "+ s.replace('H', 'M'));
+	}
+
+}
