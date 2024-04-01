d5bae73362caf76a9b823fae72755cdedde186e2
nandani109 <nandanisolanki1810@gmail.com>
2024-04-01 09:38:41 +0530

    prac29

---
+++ prac 29/src/prac29/P29.java
@@ -0,0 +1,28 @@
+package prac29;
+class multiThread extends Thread{ 
+    int num;
+    multiThread(int num){
+    this.num = num;
+    }
+    public void run(){
+        try{
+        System.out.println("Thread "+Thread.currentThread().getId()+" is created");
+        System.out.println("Number: "+num);
+        Thread.sleep(1000);
+        }
+        catch(Exception e){
+            System.out.println("Exception "+e);
+        }
+    }
+}
+
+public class P29 {
+    public static void main(String[] args) {
+        
+        for(int i = 1; i<=5; i++){
+            multiThread t = new multiThread(i);
+            t.start();
+        }
+    }
+    
+}
